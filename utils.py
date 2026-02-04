d54ff9d3f56fbe6fc3a53b7e9479eac95d7b078c
Megamaruthu5 <megavarshini400@gmail.com>
2026-02-04 17:42:51 +0530

    Added global skill database

--- utils.py
+++ utils.py
@@ -2,3 +2,24 @@ from job_roles_data import job_roles
 
 def get_skills_for_role(role):
     return job_roles.get(role, [])
+
+SKILL_DATABASE = [
+    "Python",
+    "Machine Learning",
+    "Deep Learning",
+    "Data Science",
+    "Statistics",
+    "Pandas",
+    "NumPy",
+    "SQL",
+    "NLP",
+    "Computer Vision",
+    "TensorFlow",
+    "PyTorch",
+    "Git",
+    "GitHub",
+    "Data Visualization",
+    "Matplotlib",
+    "Seaborn"
+]
+
